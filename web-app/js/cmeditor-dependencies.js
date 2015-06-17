ecf1ce46b75dc2bdc66a2498d093a65ff17ffc03
Tilman Adler <Tilman.Adler@fau.de>
2015-06-17 14:00:49 +0200

    update to codemirror 5.3

--- web-app/js/cmeditor-dependencies.js
+++ web-app/js/cmeditor-dependencies.js
@@ -1,25 +1,25 @@
 //= require jquery
 //= require jquery-ui
-//= require codemirror-4.0/lib/codemirror
+//= require codemirror-5.3/lib/codemirror
 
-//= require codemirror-4.0/mode/htmlmixed/htmlmixed
-//= require codemirror-4.0/mode/htmlembedded/htmlembedded
-//= require codemirror-4.0/mode/css/css
-//= require codemirror-4.0/mode/xml/xml
-//= require codemirror-4.0/mode/javascript/javascript
-//= require codemirror-4.0/mode/groovy/groovy
-//= require codemirror-4.0/mode/clike/clike
-//= require codemirror-4.0/mode/properties/properties
-//= require_full_tree codemirror-4.0/keymap
-//= require_full_tree codemirror-4.0/addon/dialog
-//= require_full_tree codemirror-4.0/addon/edit
-//= require_full_tree codemirror-4.0/addon/search
-//= require codemirror-4.0/addon/mode/overlay
-//= require_full_tree codemirror-4.0/addon/hint
-//= require_full_tree codemirror-4.0/addon/comment
-//= require_full_tree codemirror-4.0/addon/fold
-//= require_full_tree codemirror-4.0/addon/selection
-//= require_full_tree codemirror-4.0/addon/display
+//= require codemirror-5.3/mode/htmlmixed/htmlmixed
+//= require codemirror-5.3/mode/htmlembedded/htmlembedded
+//= require codemirror-5.3/mode/css/css
+//= require codemirror-5.3/mode/xml/xml
+//= require codemirror-5.3/mode/javascript/javascript
+//= require codemirror-5.3/mode/groovy/groovy
+//= require codemirror-5.3/mode/clike/clike
+//= require codemirror-5.3/mode/properties/properties
+//= require_full_tree codemirror-5.3/keymap
+//= require_full_tree codemirror-5.3/addon/dialog
+//= require_full_tree codemirror-5.3/addon/edit
+//= require_full_tree codemirror-5.3/addon/search
+//= require codemirror-5.3/addon/mode/overlay
+//= require_full_tree codemirror-5.3/addon/hint
+//= require_full_tree codemirror-5.3/addon/comment
+//= require_full_tree codemirror-5.3/addon/fold
+//= require_full_tree codemirror-5.3/addon/selection
+//= require_full_tree codemirror-5.3/addon/display
 
 //= require_full_tree jsdifflib
 
