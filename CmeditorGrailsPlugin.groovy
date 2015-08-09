bea6f15835db89a589af00196147529d8e1114f0
Tilman Adler <Tilman.Adler@fau.de>
2015-08-09 19:32:54 +0200

    version bump

--- CmeditorGrailsPlugin.groovy
+++ CmeditorGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class CmeditorGrailsPlugin {
-    def version = "2.4.0"
+    def version = "2.4.1"
     def grailsVersion = "2.0 > *"
     def title = "CM Editor Plugin"
     def author = "Frank Tröger"
