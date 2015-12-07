26843b3e3be14b94150bd69492a9bdad48e3fda3
Frank Tröger <frank.troeger@fau.de>
2015-12-07 16:27:19 +0100

    fix of fix#9

--- CmeditorGrailsPlugin.groovy
+++ CmeditorGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class CmeditorGrailsPlugin {
-    def version = "2.5.4"
+    def version = "2.5.5"
     def grailsVersion = "2.0 > *"
     def title = "CM Editor Plugin"
     def author = "Frank Tröger"
