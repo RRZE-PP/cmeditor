90bde398dbe668d9fc3dd50cdd90ced28c2f214e
Sven Marschke <sven.marschke@fau.de>
2016-07-19 16:14:51 +0200

    asset: changed from require_full_tree to require; bumped version to 2.6.1

--- grails-app/assets/javascripts/cmeditor-dependencies.js
+++ grails-app/assets/javascripts/cmeditor-dependencies.js
@@ -1,27 +1,55 @@
 //= require jsdifflib
 
-//= require_full_tree spin-2.3.2
+//= require spin-2.3.2/spin.min
 
-//= require_full_tree FileSaver-1.1.20150716
+//= require FileSaver-1.1.20150716/FileSaver.min
 
 //= require jquery-ui-1.10.4.custom/js/jquery-ui-1.10.4.custom.min
 
-//= require_full_tree jquery-layout-1.4.0
+//= require jquery-layout-1.4.0/jquery.layout-latest
 
 // after our pull request was merged upstream, this can be changed to dist/...min.js
 //= require jqueryfiletree-2.0.1/src/jQueryFileTree
 
 //= require codemirror-5.3/lib/codemirror
 
-//= require_full_tree codemirror-5.3/keymap
-//= require_full_tree codemirror-5.3/addon/dialog
-//= require_full_tree codemirror-5.3/addon/edit
-//= require_full_tree codemirror-5.3/addon/search
-//= require_full_tree codemirror-5.3/addon/hint
-//= require_full_tree codemirror-5.3/addon/comment
-//= require_full_tree codemirror-5.3/addon/fold
-//= require_full_tree codemirror-5.3/addon/selection
-//= require_full_tree codemirror-5.3/addon/display
+//= require codemirror-5.3/keymap/emacs
+//= require codemirror-5.3/keymap/sublime
+//= require codemirror-5.3/keymap/vim
+//= require codemirror-5.3/addon/dialog/dialog
+//= require codemirror-5.3/addon/edit/closebrackets
+//= require codemirror-5.3/addon/edit/closetag
+//= require codemirror-5.3/addon/edit/continuelist
+//= require codemirror-5.3/addon/edit/matchbrackets
+//= require codemirror-5.3/addon/edit/matchtags
+//= require codemirror-5.3/addon/edit/trailingspace
+//= require codemirror-5.3/addon/search/match-highlighter
+//= require codemirror-5.3/addon/search/matchesonscrollbar
+//= require codemirror-5.3/addon/search/search
+//= require codemirror-5.3/addon/search/searchcursor
+//= require codemirror-5.3/addon/hint/anyword-hint
+//= require codemirror-5.3/addon/hint/css-hint
+//= require codemirror-5.3/addon/hint/html-hint
+//= require codemirror-5.3/addon/hint/javascript-hint
+//= require codemirror-5.3/addon/hint/show-hint
+//= require codemirror-5.3/addon/hint/sql-hint
+//= require codemirror-5.3/addon/hint/xml-hint
+//= require codemirror-5.3/addon/comment/comment
+//= require codemirror-5.3/addon/comment/continuecomment
+//= require codemirror-5.3/addon/fold/brace-fold
+//= require codemirror-5.3/addon/fold/comment-fold
+//= require codemirror-5.3/addon/fold/foldcode
+//= require codemirror-5.3/addon/fold/foldgutter
+//= require codemirror-5.3/addon/fold/indent-fold
+//= require codemirror-5.3/addon/fold/markdown-fold
+//= require codemirror-5.3/addon/fold/xml-fold
+//= require codemirror-5.3/addon/selection/active-line
+//= require codemirror-5.3/addon/selection/mark-selection
+//= require codemirror-5.3/addon/selection/selection-pointer
+//= require codemirror-5.3/addon/display/fullscreen
+//= require codemirror-5.3/addon/display/panel
+//= require codemirror-5.3/addon/display/placeholder
+//= require codemirror-5.3/addon/display/rulers
 
 //= require codemirror-5.3/mode/meta.js
 //= require codemirror-5.3/addon/mode/overlay
